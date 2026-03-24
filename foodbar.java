f711b07538886c162b0730c4e747da72943ca6cc
LithiumIron <moonstarjarry@gmail.com>
2026-03-24 14:07:28 +0000

    testrun

---
+++ foodbar.java
@@ -0,0 +1,13 @@
+public class foodbar {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Food Bar!");
+        System.out.println("Please select a combo from the menu:");
+        System.out.println("1. Popcorn + Sprite - $13.00");
+        System.out.println("2. Hotdog x2 + Cola - $12.00");
+        System.out.println("3. French Fries + 100 Plus - $6.00");
+        System.out.println("4. Chicken Meatball + Water - $9.50");
+        System.out.println("5. Nuggets + Pepsi - $8.00");
+        System.out.print("Enter the number of your choice: ");  
+    }
+}
