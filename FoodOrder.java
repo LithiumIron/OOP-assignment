b850c9ebab34a4f347fd2fd7f56ad32840e5d163
LithiumIron <moonstarjarry@gmail.com>
2026-03-27 21:39:49 +0800

    updated

--- FoodOrder.java
+++ FoodOrder.java
@@ -1,41 +1,64 @@
-//Object ob1= {new FoodOrder(), new Combo(),...}; can make a array and call the food i think
-
+import java.util.ArrayList;
 public class FoodOrder {
-    private static int foodOrderId=0;
-    private int orderNo=0;
-    
-    @SuppressWarnings("FieldMayBeFinal")//ignore this
-    private String[] order=new String[100];
+//data properties
+    private static int orderNo=0;
+    private int itemNo=0;    
     private double totalPrice=0.0;
+    private ArrayList<String> order = new ArrayList<>();
+    
+    
+    public FoodOrder(){
+    	this(0,0.0,new ArrayList<>(),0);
+    }
+    public FoodOrder(int orderNo, double totalPrice,ArrayList<String> order,int itemNo){
+    	FoodOrder.orderNo=++orderNo;
+    	this.totalPrice=totalPrice;
+    	this.order=new ArrayList<>(order);
+    	this.itemNo=itemNo;
+    	
+    }
     
     //getter
-
-    public static int getFoodOrderId() {
-        return foodOrderId;
+    public static int getOrderNo() {
+        return orderNo;
     }
  
-    public int getOrderNo(){
-    	return orderNo;
+    public int getItemNo(){
+    	return itemNo;
     }
-    public String getOrder() {
-        StringBuilder sb = new StringBuilder();
-        for(int i=0;i<orderNo;i++){
-            sb.append(order[i]).append(", ");
-        }
-        return sb.toString();
-    }
+    
     public double getTotalPrice() {
         return totalPrice;
     }
+       
+    public String getOrder(){
+    	StringBuilder sb = new StringBuilder();
+    	for(int i=0; i<itemNo; i++){
+    		sb.append(order.get(i));
+    		if(i<itemNo-1){
+    			sb.append(", ");
+    		}//if end
+    	}//for end
+    	return sb.toString();
+    }//method end
+    
     //setter
-    public static void setFoodOrderId(int foodOrderId) {
-        FoodOrder.foodOrderId = foodOrderId;
+    public static void setOrderNo(int orderNo) {
+        FoodOrder.orderNo = orderNo;
     }
-    public void setOrder(int x, String order) {     //for programmer or sum
-        this.order[x] = order;
+    
+    public void setItemNo(int itemNo){
+    	this.itemNo=itemNo;
     }
+
     public void setTotalPrice(double totalPrice) {
         this.totalPrice = totalPrice;
+    }
+    
+    public void setOrder(ArrayList<String> order) {     //to reset & make a new orders
+        this.order.clear();
+        this.order.addAll(order);
+       	this.itemNo=this.order.size();
     }
 
     //------------other methods------------
@@ -43,15 +66,20 @@ public class FoodOrder {
     public void addOrderNo(){
     	orderNo++;
     }//add order number end
+    
+    public void addItem(String item,double price){
+    	order.add(item);
+    	totalPrice +=price;
+    	itemNo++;
+    }
 
     //cost size
-    public double largeCost(double x){
-    	return x*2;
+    public double largeCost(double price){
+    	return price*2;
     }//double end
 
-    @Override   //ignore this
     public String toString(){
-        return "Food Order ID: "+foodOrderId+
+        return "Order No: "+orderNo+
                 "\nOrder: "+getOrder()+
                 "\nCost: RM"+totalPrice;
     }
