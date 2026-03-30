3a6ed1cbf092b213a58e9a8a007d3009ce1d7fa5
LithiumIron <moonstarjarry@gmail.com>
2026-03-30 20:57:15 +0800

    Rename Test Movie&amp;Hall.java to TestMovieNHall.java

--- TestMovieNHall.java
+++ TestMovieNHall.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.List;
 
-public class MinHui {
+public class TestMovieNHall {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
