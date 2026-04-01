82b2fd5602a60868916c1097ccf28d0cdc81fabe
LithiumIron <moonstarjarry@gmail.com>
2026-04-01 10:01:12 +0800

    Update mh_main.java

--- mh_main.java
+++ mh_main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.List;
 
-public class MinHui {
+public class mh_main {
     private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
