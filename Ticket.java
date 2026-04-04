7a22a75502e12a1b536c79cc337b9f012ef66161
tayzy1231 <yongtay9283@gmail.com>
2026-04-04 18:35:48 +0800

    Ticket.java

--- Ticket.java
+++ Ticket.java
@@ -1,8 +1,8 @@
-
 public class Ticket {
 //data properties
 private String ticketID;
 private String ticketType;
+private double ticketAmt;
 
 private static int counter = 0;
 
@@ -12,12 +12,14 @@ private static int counter = 0;
 public Ticket(){
 	this.ticketID = String.format("T%03d", counter++);
 	this.ticketType = " ";
+	this.ticketAmt = 0.0;
 }
   
 //cons with args
-public Ticket(String ticketType){
+public Ticket(String ticketType, double ticketAmt){
 	this.ticketID = String.format("T%03d", counter++);
 	this.ticketType = ticketType;
+	this.ticketAmt = ticketAmt;
 
 }
 
@@ -30,36 +32,84 @@ public String getTicketType(){
 	return ticketType;
 }
 
+public double getTicketAmt(){
+	return ticketAmt;
+}
+
 
 //setter
 public void setTicketType(String ticketType){
 	this.ticketType = ticketType;
 }
 
+public void setTicketAmt(double ticketType){
+	this.ticketAmt = ticketAmt;
+}
+
+
 //------------------ other methods --------------------
 public static String ticketHeader(){
-	return String.format("\t\t%-15s \n\t\t%-15s \n\t\t%-15s\n\n%-15s %-11s\n%-15s %-11s\n", 
+	return String.format("\t\t\t\t%-15s \n\t\t\t\t%-15s \n\t\t\t\t%-15s\n\n%-15s %-17s %-12s\n%-15s %-17s %-12s\n", 
 							"========", " Ticket", "========",
-							"Ticket ID", "Ticket Type", "=========", "===========" );
+							"Ticket ID", "Ticket Type", "Ticket Price", "=========", "===========", "============" );
 }
 
 public static String noOfTickets(){
-	return "Please enter number of tickets ";
+	return "Please enter number of tickets : ";
 }
 
-public static String selectTicketType(){
-	return "Please select a ticket type\n" +
-			"1. Adult\n" +
-			"2. Child\n" +
-			"3. Student\n" +
+public static String selectHallSize(){
+	return "Please select a hall size\n" +
+			"1. Small\n" +
+			"2. Medium\n" +
+			"3. Large\n" +
+			"Enter your choice for hall size : ";
+}
+public static String selectTicketType1(){
+	return "[Small Hall] Please select a ticket type\n" +
+			"1. Adult  - RM 15.00\n" +
+			"2. Child  - RM 10.00\n" +
+			"3. Senior - RM 10.00\n" +
 			"Enter your choice for ticket ";
 } 
 
+public static String selectTicketType2(){
+	return "[Medium Hall] Please select a ticket type\n" +
+			"1. Adult  - RM 20.00\n" +
+			"2. Child  - RM 12.00\n" +
+			"3. Senior - RM 12.00\n" +
+			"Enter your choice for ticket ";
+} 
 
-public String toString(){
-	return String.format("%-15s %-11s\n",
-						ticketID, ticketType);
+public static String selectTicketType3(){
+	return "[Large Hall] Please select a ticket type\n" +
+			"1. Adult  - RM 25.00\n" +
+			"2. Child  - RM 15.00\n" +
+			"3. Senior - RM 15.00\n" +
+			"Enter your choice for ticket ";
+} 
+
+public static double calculateTicketTotal(Ticket [] tic){
+	double ticketTotal = 0.0;
+	for(Ticket t : tic){
+		ticketTotal += t.getTicketAmt();
+	}
+	return ticketTotal;	
+}
+
+public static String printTotal(Ticket [] tic){
+	return String.format("%-15s %-11s \t  RM%.2f", " ", "Total:", calculateTicketTotal(tic));
 	
 }
+
+public static String sizeInvalidMsg(){
+	return "Invalid size. Please enter again!";
+}
+public String toString(){
+	return String.format("%-15s %-17s RM%.2f\n",
+						ticketID, ticketType, ticketAmt);
+	
+}
+
     
 }
